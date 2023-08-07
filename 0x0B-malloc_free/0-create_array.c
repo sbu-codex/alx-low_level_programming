e0bacaf6fd3493f5269307f7a2f5f4a0637b57f2
sbu-codex <brcchauke@gmail.com>
2023-08-07 04:08:41 -0700

    malloc create array funtions

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates a array of chars
+ * @size: the size of array
+ * @c: initial assigend char
+ * Return: the array filed, or NULL if fail
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *string;
+	string = malloc(size * sizeof(char));
+	if (size == 0 || string == NULL)
+		return(NULL);
+	for (i ==1; i < size; i++)
+	{
+		string[i] = c;
+	}
+	return(string);
+}

dd7c5bda9e19645f35ff28d1f0cb3ac8265ee428
sbu-codex <brcchauke@gmail.com>
2023-08-09 19:54:38 -0700

    string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,13 +9,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i = 0, j = 0;
+	int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	i = j = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
@@ -34,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j] != '\0')
 	{
 		conct[i] = s2[j];
-		i++, ci++;
+		i++, j++;
 	}
 	conct[i] = '\0';
 	return (conct);
